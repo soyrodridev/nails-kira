b8e4de5841099d5065bc523b64c7f5bca0533c8c
AdolfKing20 <rodrigogfxs@gmail.com>
2026-05-06 02:54:51 -0300

    Inicio

---
+++ src/lib/supabase.js
@@ -0,0 +1,9 @@
+// src/lib/supabase.js
+import { createClient } from '@supabase/supabase-js'
+
+
+export const supabase = createClient(
+  import.meta.env.PUBLIC_SUPABASE_URL,
+  import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+)
+console.log(import.meta.env.PUBLIC_SUPABASE_URL)
---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}

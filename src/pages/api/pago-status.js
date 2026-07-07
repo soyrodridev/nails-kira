d58c2f2cd8c3b12a9db4d6c5155066ea4c9d3b1f
AdolfKing20 <rodrigogfxs@gmail.com>
2026-07-07 02:25:28 -0300

    fix

--- src/pages/api/pago-status.js
+++ src/pages/api/pago-status.js
@@ -1,28 +1,32 @@
 import { supabaseAdmin as supabase } from "../../lib/supabaseAdmin";
 
 export async function GET({ url }) {
-
   const id = url.searchParams.get("id");
 
   if (!id) {
-
     return new Response(
       JSON.stringify({
         pagado: false,
       }),
       {
         status: 400,
-      }
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
     );
-
   }
 
-  const { data } = await supabase
-    .from("pagos_mp")
-    .select("*")
+  const { data, error } = await supabase
+    .from("pagos_pos")
+    .select("id")
     .eq("catalogo_id", id)
-    .eq("estado", "approved")
+    .eq("estado", "aprobado")
     .maybeSingle();
+
+  if (error) {
+    console.error(error);
+  }
 
   return new Response(
     JSON.stringify({
@@ -32,6 +36,6 @@ export async function GET({ url }) {
       headers: {
         "Content-Type": "application/json",
       },
-    }
+    },
   );
 }

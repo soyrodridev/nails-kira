d77e18ee8e07a2103fbd99a9f395c5b1303ae2ec
AdolfKing20 <rodrigogfxs@gmail.com>
2026-07-11 21:30:00 -0300

    fix sidebar catalogo

--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -41,12 +41,19 @@ export default function Sidebar({
       name: "Catalogos",
       href: "/admin/catalogos",
       icon: (
-        <svg  className="w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-menu-3">
-	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-	<path d="M10 6h10" />
-	<path d="M4 12h16" />
-	<path d="M7 12h13" />
-	<path d="M4 18h10" />
+        <svg 
+  className="w-5 h-5" 
+  viewBox="0 0 24 24" 
+  fill="none" 
+  stroke="currentColor" 
+  strokeWidth="2" 
+  strokeLinecap="round" 
+  strokeLinejoin="round"
+>
+  <path d="M10 6h10" />
+  <path d="M4 12h16" />
+  <path d="M7 12h13" />
+  <path d="M4 18h10" />
 </svg>
       ),
     },
